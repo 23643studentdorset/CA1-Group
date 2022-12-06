d47c82b7b3c180653b39c6e77a9f46406e9ce0a1
23643@student.dorset-college.ie <23643@student.dorset-college.ie>
2022-12-06 01:22:27 +0000

    acelerometer/score sent to firebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,40 +1,61 @@
-import { StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet, TextInput, View, Text } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { auth, db } from '../firebase'
-import { signOut } from "firebase/auth";
+import { signOut, getAuth} from "firebase/auth";
 import CustomButton from '../components/CustomButton';
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc, updateDoc  } from "firebase/firestore"; 
 import { Accelerometer } from 'expo-sensors';
 
 const HomeScreen = () => {
   const [name, setName] = useState('')
   const [course, setCourse] = useState('')
-  const [year, setYear] = useState('')
+  const [year, setYear] = useState()
   const [id, setId] = useState('')
-  const [score, setScore] = useState('')
+  const [score, setScore] = useState()
+  const [preScore, setPreScore] = useState(0)
   const navigation = useNavigation()
+  
+  //set initial values
+  useEffect(()=>{
+    setAccelometerArray([])
+    setAccelerometerCounter(0)
+    setScore(0)
+    Accelerometer.addListener(setAccelerometerData);
+    Accelerometer.setUpdateInterval(200)
+  }, [])
 
-  //acelerometer
-  const [{ x, y, z }, setAcelerometerData] = useState({x: 0, y: 0, z: 0,});
-  const [subscription, setSubscription] = useState(null);
-  const _fast = () => Accelerometer.setUpdateInterval(200)
-  const _subscribe = () => {
-    setSubscription(
-      Accelerometer.addListener(setAcelerometerData)
-    );
-  };
+  //Accelerometer
+  const [accelerometerData, setAccelerometerData] = useState({x: 0, y: 0, z: 0,})
+  const [accelerometerCounter, setAccelerometerCounter] = useState(0)
+  const [accelerometerArray, setAccelometerArray] = useState([])
+  
+  useEffect(()=>{
+    setAccelerometerCounter(accelerometerCounter + 1)
+    setAccelometerArray(current => [...current, accelerometerData]);
+    setPreScore(preScore + calculateScore())
+    if (accelerometerCounter === 1000 && id != '')
+    {
+      setScore(preScore/accelerometerCounter)
+      console.log(Score)
+      sendAccelometerData(accelerometerArray, score, id)
+      setAccelerometerCounter(0)
+      setAccelometerArray([])
+      setScore(0)
+    }    
+  }, [accelerometerData]);
 
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
+  async function sendAccelometerData(accelerometer_data, score, id){
+    await updateDoc(doc(db, "Firebase", id), {
+      accelerometer_data: accelerometer_data,
+      score: score
+    });
+  }
 
-
+  //score calculation
+  function calculateScore(){
+    return Math.abs(accelerometerData.x) + Math.abs(accelerometerData.y) + Math.abs(accelerometerData.z)
+  }
 
   //button handlers
   const handleSignOut = () => {
@@ -46,38 +67,26 @@ const HomeScreen = () => {
   }
  
   const handlePress = () =>{
-    setAcelerometerData({})
-    setScore(1)
-    handleData(id, name, course, year, acelerometerData, score)
+    handleData(id, name, course, year, [], score)
   }
 
   //send info to firebase
-  async function handleData(id, name, course, year, acelerometerData, score) {
+  async function handleData(id, name, course, year, accelerometer_data, score) {
     await setDoc(doc(db, "Firebase", id), {
       name: name, 
       course: course, 
       year: Number(year),
-      acelerometerData: acelerometerData, 
+      accelerometer_data: accelerometer_data, 
       score: Number(score),
     });
   }
+  
   return (
-    <View style={styles.container}>
-        
-      <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} style={styles.button}>
-          <Text>Fast</Text>
-        </TouchableOpacity>
-      </View>
-        
-        
+        <View style={styles.container}>
+          <Text style={styles.text}>counter: {accelerometerCounter}</Text>
+          <Text style={styles.text}></Text>
+          <View style={styles.buttonContainer}>        
+        </View>
         <TextInput
           placeholder="Name"
           value= {name}
@@ -96,22 +105,24 @@ const HomeScreen = () => {
           onChangeText={text => setYear(text)}
           style={styles.input}
         />
-
         <TextInput
           placeholder="student ID"
           value= {id}
           onChangeText={text => setId(text)}
           style={styles.input}
         />
-
-      <CustomButton
-        onPress={handleSignOut}
-        name='Sign out'
+        <CustomButton
+          onPress={handleSignOut}
+          name='Sign out'
+          />
+        <CustomButton
+          onPress={handlePress}
+          name='Send data'
         />
-      <CustomButton
-        onPress={handlePress}
-        name='Send data'
-      />
+        <CustomButton 
+          onPress={navigation.replace("Login")}
+          name= 'Leader board'
+        />
     </View>
   )
 }
--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -1,15 +1,24 @@
+import { StyleSheet, TextInput, View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import { TextInput, View, StyleSheet, KeyboardAvoidingView } from 'react-native'
-import React, { useState } from 'react'
-import {createUserWithEmailAndPassword} from "firebase/auth";
+import { auth, db } from '../firebase'
+import { signOut, getAuth} from "firebase/auth";
 import CustomButton from '../components/CustomButton';
-import { auth } from '../firebase'
+import { collection, getDocs  } from "firebase/firestore"; 
+import { Accelerometer } from 'expo-sensors';
 
 
-const RegisterScreen = () => {
+const LeaderboardScreen = () => {
+    const querySnapshot = async () => {await getDocs(collection(db, "Firebase"))};
+    
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
 
     return(
         <>
         </>
     )
 }
+
+export default LeaderboardScreen
