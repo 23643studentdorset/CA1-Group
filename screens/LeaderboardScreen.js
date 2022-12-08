c019efc2fd8405a8f6db9e867638ce6dc2735939
23643@student.dorset-college.ie <23643@student.dorset-college.ie>
2022-12-08 16:21:23 +0000

    learderboars with users and score data

--- screens/LeaderboardScreen.js
+++ screens/LeaderboardScreen.js
@@ -4,19 +4,52 @@ import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase'
 
 const LeaderboardScreen = () => {
-  
+const [scores, setScores] = useState([])
+const [leaderboardData, setLearderboarData] = useState([])
+const [singleScore, setSingleScore] = useState(0)
+
+
   //grab users from firestore
   const [users, setUsers] = useState([])
   const usersCollectionRef = collection(db, "Firestore")
+  
   useEffect(()=>{
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc)=> ({...doc.data(), id: doc.id})))
-      console.log(users)
+      const users = data.docs.map((doc)=> ({...doc.data(), id: doc.id})) 
+      setUsers(users)      
+      //console.log(users)
+      const leaderboardData = users.map((n)=> n.accelerometer_data);
+      setLearderboarData(leaderboardData);
+      //console.log(leaderboardData)
+      setScores([])
+      leaderboardData.forEach(arrayElement => {
+        //console.log(arrayElement)
+        let score = 0
+        if (arrayElement.length === 1000){
+          //console.log("I'm here")
+          arrayElement.forEach(objectElement => {
+            //console.log(objectElement)
+            score += calculateScore(objectElement)
+            //setSingleScore(singleScore + calculateScore(objectElement))
+          });
+        score = score/1000
+        setSingleScore(score)
+        //console.log("score:" + score)
+        scores.push(score);  
+        }else {
+          scores.push (-1)
+        }          
+      });
+        console.log(scores)
     }
     getUsers()
-  }, [users])
-  
+  }, [])
+
+  function calculateScore(object){
+    const calculate = Math.abs(object.x) + Math.abs(object.y) + Math.abs(object.z)
+    return calculate
+  }
 
   return(
     <View style={styles.container2}>
